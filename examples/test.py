9384ea82108e38cdab0fb00eda9b00bf2c6d68db
Jakob Schlyter <jakob@kirei.se>
2021-03-17 17:47:30 +0100

    reformat

--- examples/test.py
+++ examples/test.py
@@ -5,9 +5,11 @@ from chargeamps.external import ChargeAmpsExternalClient
 
 
 async def test():
-    with open('credentials.json') as input_file:
+    with open("credentials.json") as input_file:
         c = json.load(input_file)
-    client = ChargeAmpsExternalClient(email=c['username'], password=c['password'], api_key=c['api_key'])
+    client = ChargeAmpsExternalClient(
+        email=c["username"], password=c["password"], api_key=c["api_key"]
+    )
 
     chargepoints = await client.get_chargepoints()
     print(chargepoints)
@@ -16,7 +18,9 @@ async def test():
     print("Status:", status)
 
     for c in status.connector_statuses:
-        settings = await client.get_chargepoint_connector_settings(c.charge_point_id, c.connector_id)
+        settings = await client.get_chargepoint_connector_settings(
+            c.charge_point_id, c.connector_id
+        )
         print("Before:", settings)
         settings.max_current = 6
         await client.set_chargepoint_connector_settings(settings)
@@ -29,4 +33,3 @@ if __name__ == "__main__":
     # asyncio.run(test())
     loop = asyncio.get_event_loop()
     loop.run_until_complete(test())
-
