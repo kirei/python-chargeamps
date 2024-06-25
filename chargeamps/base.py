cb28dcdb556f79808d07b724aea7c019f71251e5
Jakob Schlyter <jakob@kirei.se>
2024-06-25 09:53:42 +0200

    remove methods only applicable to external API

--- chargeamps/base.py
+++ chargeamps/base.py
@@ -1,16 +1,12 @@
 """Base class for ChargeAmps API"""
 
 from abc import ABCMeta, abstractmethod
-from datetime import datetime
-from typing import Optional
 
 from .models import (
     ChargePoint,
     ChargePointConnectorSettings,
     ChargePointSettings,
     ChargePointStatus,
-    ChargingSession,
-    StartAuth,
 )
 
 
@@ -22,23 +18,6 @@ class ChargeAmpsClient(metaclass=ABCMeta):
     @abstractmethod
     async def get_chargepoints(self) -> list[ChargePoint]:
         """Get all owned chargepoints"""
-        pass
-
-    @abstractmethod
-    async def get_all_chargingsessions(
-        self,
-        charge_point_id: str,
-        start_time: Optional[datetime] = None,
-        end_time: Optional[datetime] = None,
-    ) -> list[ChargingSession]:
-        """Get all charging sessions"""
-        pass
-
-    @abstractmethod
-    async def get_chargingsession(
-        self, charge_point_id: str, session: int
-    ) -> ChargingSession:
-        """Get charging session"""
         pass
 
     @abstractmethod
@@ -70,16 +49,4 @@ class ChargeAmpsClient(metaclass=ABCMeta):
         self, settings: ChargePointConnectorSettings
     ) -> None:
         """Get all owned chargepoints"""
-        pass
-
-    @abstractmethod
-    async def remote_start(
-        self, charge_point_id: str, connector_id: int, start_auth: StartAuth
-    ) -> None:
-        """Remote start chargepoint"""
-        pass
-
-    @abstractmethod
-    async def remote_stop(self, charge_point_id: str, connector_id: int) -> None:
-        """Remote stop chargepoint"""
         pass
